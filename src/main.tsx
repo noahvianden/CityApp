e1ef05b7a63daee860135ab495614863c5c3ce08
noahvianden <31614921+noahvianden@users.noreply.github.com>
2026-05-09 16:04:10 +0200

    Wire generated city creator into app startup

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import GeneratedCityPanel from './GeneratedCityPanel.tsx'
 import MobileDiagnosticsPanel from './MobileDiagnosticsPanel.tsx'
+import { bootstrapGeneratedCities } from './generatedCityBootstrap.ts'
 import { bootstrapNativeSnapshotIntoLocalStorage, startNativeSnapshotMirror } from './mobileSnapshotStore.ts'
 import './mobileVisualPolish.css'
 import './visualSpriteSystem.css'
 
 async function startApp() {
   await bootstrapNativeSnapshotIntoLocalStorage()
+  bootstrapGeneratedCities()
   startNativeSnapshotMirror()
 
   createRoot(document.getElementById('root')!).render(
     <StrictMode>
       <App />
+      <GeneratedCityPanel />
       <MobileDiagnosticsPanel />
     </StrictMode>,
   )
