ee8215c419bd2d9d3eeec08a2095507e5a4419a2
noahvianden <31614921+noahvianden@users.noreply.github.com>
2026-05-09 14:34:12 +0200

    Bootstrap native snapshot storage before render

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import MobileDiagnosticsPanel from './MobileDiagnosticsPanel.tsx'
+import { bootstrapNativeSnapshotIntoLocalStorage, startNativeSnapshotMirror } from './mobileSnapshotStore.ts'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-    <MobileDiagnosticsPanel />
-  </StrictMode>,
-)
+async function startApp() {
+  await bootstrapNativeSnapshotIntoLocalStorage()
+  startNativeSnapshotMirror()
+
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+      <MobileDiagnosticsPanel />
+    </StrictMode>,
+  )
+}
+
+void startApp()
