99062a9db3d79c064ccc763bf544fcf9661cbff2
Noah Vianden <noah.vianden@gmail.com>
2026-05-11 00:59:59 +0200

    Remove unused non-atlas source files

--- src/navigation.ts
+++
@@ -1,7 +0,0 @@
-import type { City, Place } from './cityprintData'
-
-export function buildExternalNavigationUrl(city: Pick<City, 'name' | 'country'>, place: Pick<Place, 'name' | 'district'>) {
-  const query = [place.name, place.district, city.name, city.country].filter(Boolean).join(', ')
-
-  return `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(query)}`
-}
