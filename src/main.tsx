e165f6a63e33ebf4eaf95269f72c3b8fe4fdc199
noahvianden <31614921+noahvianden@users.noreply.github.com>
2026-05-11 17:35:17 +0200

    Enable atlas fog overlay

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import FoggedApp from './FoggedApp.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <FoggedApp />
   </StrictMode>,
 )
