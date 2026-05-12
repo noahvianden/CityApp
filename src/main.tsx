85dbc35c8cd9107efda202df3705d5458078e0d3
noahvianden <31614921+noahvianden@users.noreply.github.com>
2026-05-12 22:46:08 +0200

    Install atlas map session bridge

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { installAtlasMapSessionBridge } from './atlasMapSessionBridge'
 import { installLiveWalkDomBridge } from './liveWalkDomBridge'
 import { installLiveWorldPlacesBridge } from './liveWorldPlacesBridge'
 import { installPlaceDiscoveryOverlayBridge } from './placeDiscoveryOverlayBridge'
@@ -10,6 +11,7 @@ import FoggedApp from './FoggedApp.tsx'
 console.info('[cityprint-build] live-world-places-v1')
 installLiveWalkDomBridge()
 installRealTabContentBridge()
+void installAtlasMapSessionBridge()
 void installPlaceDiscoveryOverlayBridge().then(() => installLiveWorldPlacesBridge())
 
 createRoot(document.getElementById('root')!).render(
