ef8f3baac39159c96a284655c67f35780bf5f931
noahvianden <31614921+noahvianden@users.noreply.github.com>
2026-05-12 17:55:34 +0200

    Install live walk bridge

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { installLiveWalkDomBridge } from './liveWalkDomBridge'
 import FoggedApp from './FoggedApp.tsx'
+
+installLiveWalkDomBridge()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
