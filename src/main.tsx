9ac873eb24e932441183b7367af20f2e885ac1f2
noahvianden <31614921+noahvianden@users.noreply.github.com>
2026-05-12 20:24:00 +0200

    Install clickable place discovery cards

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { installLiveWalkDomBridge } from './liveWalkDomBridge'
 import { installLiveWorldPlacesBridge } from './liveWorldPlacesBridge'
+import { installPlaceDiscoveryOverlayBridge } from './placeDiscoveryOverlayBridge'
 import FoggedApp from './FoggedApp.tsx'
 
 console.info('[cityprint-build] live-world-places-v1')
 installLiveWalkDomBridge()
-void installLiveWorldPlacesBridge()
+void installPlaceDiscoveryOverlayBridge().then(() => installLiveWorldPlacesBridge())
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
