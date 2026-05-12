a2e37cffefba4cabbf0c11429b208c45878a3584
noahvianden <31614921+noahvianden@users.noreply.github.com>
2026-05-12 21:33:01 +0200

    Install real tab content bridge

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import './index.css'
 import { installLiveWalkDomBridge } from './liveWalkDomBridge'
 import { installLiveWorldPlacesBridge } from './liveWorldPlacesBridge'
 import { installPlaceDiscoveryOverlayBridge } from './placeDiscoveryOverlayBridge'
+import { installRealTabContentBridge } from './realTabContentBridge'
 import FoggedApp from './FoggedApp.tsx'
 
 console.info('[cityprint-build] live-world-places-v1')
 installLiveWalkDomBridge()
+installRealTabContentBridge()
 void installPlaceDiscoveryOverlayBridge().then(() => installLiveWorldPlacesBridge())
 
 createRoot(document.getElementById('root')!).render(
